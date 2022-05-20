43d61dbc4edd5ddc094ed81b5bedf5f9ed21adc8
Ia <iavaquilar@gmail.com>
2022-05-20 10:08:45 +0100

    Changed Groups to Enum

--- MathemathicsClass/MathemathicsClass/StudentTable.cs
+++ MathemathicsClass/MathemathicsClass/StudentTable.cs
@@ -10,7 +10,16 @@ namespace MathemathicsClass
     public class StudentTable
     {
         public enum Grades {F, E, D, C, B, A}
-        public static List<int> Groups = Enumerable.Range(1, 5).ToList();
+        //public static List<int> Groups = Enumerable.Range(1, 5).ToList();
+
+        public enum Groups
+        {
+            one = 1, 
+            two = 2, 
+            three = 3, 
+            four = 4, 
+            five = 5
+        }
 
     }
 }
